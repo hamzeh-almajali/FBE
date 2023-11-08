2e218c9a4e7aad87b746b9f66891c5f8a7d3abab
v <hamzehalmajali@icloud.com>
2023-11-08 19:17:23 +0300

    js tasks

---
+++ js/main.js
@@ -0,0 +1,118 @@
+// task 1
+// let userinput =prompt("enter your month");
+// let month="";
+// switch (userinput){
+//     case '1':
+//         month = "jan";
+//         break;
+//     case '2':
+//         month="Feb";
+//         break;
+//     case '3':
+//         month="mar";
+//         break;
+//     case '4':
+//         month="april";
+//         break;
+//     case '5':
+//         month="may" ;
+//         break;
+//     case '6':
+//         month="june" ;
+//         break;
+//     case '7':
+//         month="july" ;
+//         break;
+//     case '8':
+//         month="august";
+//         break; 
+//     case '9':
+//         month="sep";
+//         break; 
+//     case '10':
+//         month="oct" ;
+//         break
+//     case '11':
+//         month="nov" ;
+//         break;
+//     case '12':
+//         month=("Dec");
+//         break;
+//     default:
+//         console.log("enter num between 1-12");
+// }
+// console.log(month);
+// task 2
+
+// number=1;
+// for(let i=1;i<=4;i++){
+//     let row="";
+//     for(let j=0;j<i;j++){
+//         row +=number +" ";
+//         number++;
+//     }
+//     console.log(row);
+
+// };
+// task 3
+// for(i=1;i<=1000;i++){
+//     if(i%13==0){
+//         console.log(i);
+//     }
+
+
+// }
+// task4 
+// function multiply2(x,y){
+//     return x*y;
+// }
+// console.log(multiply2(4,4));
+// task 5
+
+// function triarea(x,y){
+// return 2*(x*y);
+// }
+// console.log(triarea(5,5));
+// task 6
+// let n =Number(prompt("enter a number"));
+// let nstr=n.toString();
+// let arr=nstr.split("");
+// let set1 = new Set(arr);
+// if(set1.size===10){
+//     document.write("true");
+// }
+// else{
+//     document.write("false");
+// }
+// let hh= new set(arr);
+// if(hh.size==10){
+// console.log("true");
+
+// }
+// else{
+//     console.log("false");
+// }
+
+
+// task 7
+// let user=prompt("enter your fav drink");
+// let price=0;
+// switch(user){
+//     case 'tea':
+//         price=20;
+//         break;
+//     case 'coffie':
+//         price=30;
+//     case 'orange':
+//         price=40;        
+
+
+// }
+// console.log(`the price of your drink (${user}) is ${price} `);
+// task 8
+// function fullName(x,y){
+// return x+' '+y;
+
+
+// }
+// console.log(fullName('hamzeh','almajali'));
